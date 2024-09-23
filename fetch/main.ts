97e44a7fa894aa1d5b53d474f38dd02e599e83e0
钟孟捷 <718335940@qq.com>
2024-09-24 04:17:53 +0800

    i

--- fetch/main.ts
+++ fetch/main.ts
@@ -3,5 +3,8 @@ import type { Handler } from '@/types.ts'
 export const handler: Handler = (req) => {
   const m = req.url.match(/\/fetch\/+(.*)/)
   if (!m) return undefined
+  req.headers.delete('host')
+  req.headers.delete('traceparent')
+  req.headers.delete('cdn-loop')
   return fetch(m[1], req)
 }
