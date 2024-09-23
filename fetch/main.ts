240ee02ea1099eabd9bf52664332c2219d734910
钟孟捷 <718335940@qq.com>
2024-09-24 04:08:31 +0800

    fetch

--- fetch/main.ts
+++ fetch/main.ts
@@ -3,9 +3,5 @@ import type { Handler } from '@/types.ts'
 export const handler: Handler = (req) => {
   const m = req.url.match(/\/fetch\/+(.*)/)
   if (!m) return undefined
-  return Response.json({
-    url: m[1],
-    method: req.method,
-    headers: Object.fromEntries(req.headers),
-  })
+  return fetch(m[1], req)
 }
