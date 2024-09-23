c000d8c7a98da4ef9f19baeadfae6e50dd9485f6
钟孟捷 <718335940@qq.com>
2024-09-24 04:25:45 +0800

    i

--- fetch/main.ts
+++ fetch/main.ts
@@ -1,10 +1,13 @@
 import type { Handler } from '@/types.ts'
 
+const headersToExclude = new Set(['host', 'traceparent', 'cdn-loop'])
+
 export const handler: Handler = (req) => {
   const m = req.url.match(/\/fetch\/+(.*)/)
   if (!m) return undefined
-  req.headers.delete('host')
-  req.headers.delete('traceparent')
-  req.headers.delete('cdn-loop')
-  return fetch(m[1], req)
+  return fetch(m[1], {
+    method: req.method,
+    headers: [...req.headers].filter(([k]) => !headersToExclude.has(k)),
+    body: req.body,
+  })
 }
