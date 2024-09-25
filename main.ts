455632c046459ecf34feb20027caaf6758c89109
钟孟捷 <718335940@qq.com>
2024-09-25 23:32:39 +0800

    port: 8100

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ const handlerMap: Map<string, string | Handler> = new Map([
   ['fetch', 'fetch/main.ts'],
 ])
 
-Deno.serve(async (req, info) => {
+Deno.serve({ port: 8100 }, async (req, info) => {
   const url = new URL(req.url)
   const key = url.pathname.match(/[^/]+/)?.[0]
   let handler
